e93108fa24ad91864e25bb8e82b55e32d9ae8199
Rosen Kunev <rkunev@gmail.com>
2017-11-05 17:05:41 +0200

    refactor(component): Use escaped new lines instead of string concat

--- src/color-picker.component.js
+++ src/color-picker.component.js
@@ -4,12 +4,14 @@ export default {
         color: '<?ngModel' // initial color to load
     },
     controller: ColorPickerController,
-    template:   '<div class="rotator" color-picker-rotator on-rotate="$ctrl.onRotatorDrag(angle)" angle="$ctrl.angle">' +
-                    '<div class="knob"></div>' +
-                '</div>' +
-                '<button type="button" class="color" ng-click="$ctrl.onColorSelClick()"></button>' +
-                '<div class="color-shadow"></div>' +
-                '<div class="color-palette"></div>'
+    template: '\
+        <div class="rotator" color-picker-rotator on-rotate="$ctrl.onRotatorDrag(angle)" angle="$ctrl.angle"> \
+            <div class="knob"></div> \
+        </div> \
+        <button type="button" class="color" ng-click="$ctrl.onColorSelClick()"></button> \
+        <div class="color-shadow"></div> \
+        <div class="color-palette"></div> \
+    '
 };
 
 ColorPickerController.$inject = ['$element', '$rootScope', 'ColorPickerService'];
